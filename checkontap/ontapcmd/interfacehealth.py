a8fd55d8030396aad1514f09254eaa32cf40bb4b
hias <hias@consol.de>
2025-05-22 15:32:25 +0200

    add FcInterfaces to interface-health mode

--- checkontap/ontapcmd/interfacehealth.py
+++ checkontap/ontapcmd/interfacehealth.py
@@ -17,7 +17,7 @@
 
 import logging
 from monplugin import Check,Status
-from netapp_ontap.resources import IpInterface,Svm
+from netapp_ontap.resources import IpInterface,FcInterface,Svm
 from netapp_ontap.error import NetAppRestError
 from ..tools import cli
 from ..tools.helper import setup_connection,item_filter,severity
@@ -58,35 +58,63 @@ def run():
     SvmInt = []
     # check for running svm's
     try:
-        svm = Svm.get_collection(fields="name,state,ip_interfaces")
+        svm = Svm.get_collection(fields="name,state,ip_interfaces,fc_interfaces")
         for s in svm:
             if hasattr(s, 'ip_interfaces') and 'stopped' in s.state:
                 for int in s.ip_interfaces:
                     logger.info(f"found int {int.name} on stopped svm {s.name}")
                     SvmInt.append(int.name)
+            elif hasattr(s, 'fc_interfaces') and 'stopped' in s.state:
+                for int in s.fc_interfaces:
+                    logger.info(f"found int {int.name} on stopped svm {s.name}")
+                    SvmInt.append(int.name)
+                
+            
     except NetAppRestError as error:
         check.exit(Status.UNKNOWN, f"Error => {error}")
     except Exception as error:
         check.exit(Status.UNKNOWN, f"{error}")
     
-    IpInts = []
+    Ints = []
+    
     try:
+        ##
+        ## IP Interfaces
+        ##
         interface_count = IpInterface.count_collection()
-        logger.info(f"found {interface_count} interfaces")
-        if interface_count == 0:
-            check.exit(Status.UNKNOWN, "no interfaces found")
-
-        for IpInt in IpInterface.get_collection(fields="*"):
-            if (args.exclude or args.include) and item_filter(args,IpInt.name):
-                logger.info(f"exclude interface {IpInt.name} due to include / exclude")
-                continue
-            if args.exclude_svm and hasattr(IpInt, 'svm'):
-                if re.search(args.exclude_svm, IpInt.svm.name):
-                    logger.info(f"exclude interface {IpInt.name} due to SVM exclude. SVM {IpInt.svm.name}")
+        logger.info(f"found {interface_count} ip interfaces")
+        if interface_count >= 1:
+            for IpInt in IpInterface.get_collection(fields="*"):
+                if (args.exclude or args.include) and item_filter(args,IpInt.name):
+                    logger.info(f"exclude interface {IpInt.name} due to include / exclude")
                     continue
-            logger.debug(f"INTERFACE {IpInt.name}\n{IpInt}")
-            IpInts.append(IpInt)
+                if args.exclude_svm and hasattr(IpInt, 'svm'):
+                    if re.search(args.exclude_svm, IpInt.svm.name):
+                        logger.info(f"exclude interface {IpInt.name} due to SVM exclude. SVM {IpInt.svm.name}")
+                        continue
+                logger.debug(f"INTERFACE {IpInt.name}\n{IpInt}")
+                Ints.append(IpInt)
 
+        ##
+        ## FC Interfaces
+        ##
+        
+        fcinterface_count = FcInterface.count_collection()
+        logger.info(f"found {fcinterface_count} fc interfaces")
+        if fcinterface_count >= 1:
+            for FcInt in FcInterface.get_collection(fields="*"):
+                if (args.exclude or args.include) and item_filter(args, FcInt.name):
+                    logger.info(f"exclude interface {FcInt.name} due to include / exclude")
+                    continue
+                if args.exclude_svm and hasattr(FcInt, 'svm'):
+                    if re.search(args.exclude_svm, FcInt.svm.name):
+                        logger.info(f"exclude interface {FcInt.name} due to SVM exclude. SVM {FcInt.svm.name}")
+                        continue
+                    
+                logger.debug(f"INTERFACE {FcInt.name}\n{FcInt}")
+                Ints.append(FcInt)
+
+            
     except NetAppRestError as error:
         check.exit(Status.UNKNOWN, f"Error => {error}")
     except Exception as error:
@@ -94,25 +122,39 @@ def run():
 
     count = 0
 
-    for Int in IpInts:
+    # check for state (up/down), enabled (true/false) and home node
+    for Int in Ints:
         if not Int.enabled:
             logger.info(f"Interface {Int.name} is not enabled, ignore")
             continue
-        count += 1
         if 'down' in Int.state and Int.name in SvmInt:
             logger.info(f"Interface {Int.name} for stopped svm {Int.svm.name}")
+            continue
         elif 'down' in Int.state:
             check.add_message(Status.CRITICAL, f"int {Int.name} is {Int.state}")
+            continue
+        
+        count += 1
         if not Int.location.is_home:
             check.add_message(Status.CRITICAL, f"Int {Int.name} is on {Int.location.node.name} but should be on {Int.location.home_node.name}")
 
-    check.add_message(Status.OK, f"{count} of {len(IpInts)} Interfaces are up")
+    check.add_message(Status.OK, f"{count} up / {len(Ints) - count} down - {len(Ints)} Total ({interface_count} ip / {fcinterface_count} fc at all)")
     
-    for Int in IpInts:
-        if hasattr(Int, 'svm'):
-            check.add_message(Status.OK, f"Int {Int.name:40}{Int.state:5}{Int.ip.address:16}/{Int.ip.netmask:3} is homed {Int.location.is_home} and belongs to SVM {Int.svm.name}")
+    for Int in Ints:
+        if hasattr(Int, 'ip'):
+            ipaddress = Int.ip.address
+            ipnetmask = Int.ip.netmask
         else:
-            check.add_message(Status.OK, f"Int {Int.name:40}{Int.state:5}{Int.ip.address:16}/{Int.ip.netmask:3} is homed {Int.location.is_home}")
+            ipaddress = "no_ip"
+            ipnetmask = ""
+        if hasattr(Int.location, 'is_home' ):
+            ishome = Int.location.is_home
+        else:
+            ishome = None
+        if hasattr(Int, 'svm'):
+            check.add_message(Status.OK, f"Int {Int.name:40}{Int.state:5}{ipaddress:16}/{ipnetmask:3} is homed {ishome} and belongs to SVM {Int.svm.name}")
+        else:
+            check.add_message(Status.OK, f"Int {Int.name:40}{Int.state:5}{ipaddress:16}/{ipnetmask:3} is homed {ishome}")
         
     (code, message) = check.check_messages(separator='\n  ')
     check.exit(code=code,message=message)
