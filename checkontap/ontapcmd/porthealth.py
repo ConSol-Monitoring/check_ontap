7234590b7a0b2e47ad71080e5aa02a04084fed67
hias <hias@consol.de>
2025-05-22 13:58:31 +0200

    add FcPort to port-health mode

--- checkontap/ontapcmd/porthealth.py
+++ checkontap/ontapcmd/porthealth.py
@@ -17,7 +17,7 @@
 
 import logging
 from monplugin import Check,Status
-from netapp_ontap.resources import Port
+from netapp_ontap.resources import Port,FcPort
 from netapp_ontap.error import NetAppRestError
 from ..tools import cli
 from ..tools.helper import setup_connection,item_filter,severity
@@ -26,20 +26,7 @@ __cmd__ = "port-health"
 description = f"check port status"
 
 """
-Port({
-    'mtu': 1500,
-    'uuid': 'e5b8287b-b111-11ed-975c-d039ea958568',
-    '_links': {'self': {'href': '/api/network/ethernet/ports/e5b8287b-b111-11ed-975c-d039ea958568'}},
-    'reachability': 'not_repairable',
-    'state': 'down',
-    'mac_address': 'd0:39:ev:95:x5:5e',
-    'type': 'physical',
-    'node': {
-        'uuid': '12c08d1a-e131-11ec-9572-d039ea958568',
-        '_links': {'self': {'href': '/api/cluster/nodes/12c08d1a-e131-11ec-9572-d039ea958568'}},
-        'name': 'Cluster01-Node02'},
-    'enabled': False,
-    'name': 'e0e'})
+Check Port and FcPort endpoints. Disabled interfaces are ignored.
 """
 
 def run():
@@ -62,24 +49,90 @@ def run():
     setup_connection(args.host, args.api_user, args.api_pass, args.port)
 
     try:
-        port_count = Port.count_collection()
+        ##
+        ## fibre channel ports
+        ##
+        """
+        The operational state of the FC port. - startup - The port is booting up. - link_not_connected - The port has finished initialization, but a link with the fabric is not established. - online - The port is initialized and a link with the fabric has been established. - link_disconnected - The link was present at one point on this port but is currently not established. - offlined_by_user - The port is administratively disabled. - offlined_by_system - The port is set to offline by the system. This happens when the port encounters too many errors. - node_offline - The state information for the port cannot be retrieved. The node is offline or inaccessible.
+
+        Valid choices:
         
-        if port_count == 0:
-            check.exit(Status.UNKNOWN,"no ports found on device")
+        startup
+        link_not_connected
+        online
+        link_disconnected
+        offlined_by_user
+        offlined_by_system
+        node_offline
+        unknown
+
+        """
+        FcPortOk = ['online']
+        FcPortWarning = ['startup','link_disconnected']
+        FcPortCritical = ['node_offline','offlined_by_system']
+        FcPortUnknown = ['unknown']
+        # Not interessted in link_not_connected or offlined_by_user
+       
+        fcport_count = FcPort.count_collection()
+        if fcport_count == 0:
+            check.add_message(Status.UNKNOWN,"no fc-ports found on device")
             
+        FcPorts = FcPort.fast_get_collection(fields="*")
+        # count of disabled ports 
         disabled = 0
         
-        Ports = Port.get_collection(fields="*")
-        
-        for p in Ports:
-            if not hasattr(p, "enabled") or not p.enabled:
-                logger.info(f"Port isn't enabled {p.name}")
+        for fc in FcPorts:
+            # filter
+            if (args.exclude or args.include) and item_filter(args,fc.name):
+                logger.info(f"exclude port  {fc.name}")
+                fcport_count -= 1
+                continue
+
+            # Just use real fibre channel ports
+            if fc.physical_protocol != "fibre_channel":
+                logger.info(f"FcPort isn't a fibre channel {fc.name} {fc.physical_protocol}")
+                fcport_count -= 1
+                continue
+                
+            # not enabled or disabled by user     
+            if not hasattr(fc, "enabled") or not fc.enabled or 'offlined_by_user' in fc.state:
+                logger.info(f"FcPort isn't enabled {fc.name}")
                 disabled += 1
                 continue
 
+            logger.info(f"checking {fc.node['name']} - {fc.name} - {fc.physical_protocol}")
+            logger.debug(f"{fc}")
+            out = f"{fc.physical_protocol} {fc.name} on node {fc.node['name']} is {fc.state}"
+            
+            if fc.state in FcPortOk:
+                check.add_message(Status.OK, out)
+            elif fc.state in FcPortWarning:
+                check.add_message(Status.WARNING, out)
+            elif fc.state in FcPortCritical:
+                check.add_message(Status.CRITICAL, out)
+            elif fc.state in FcPortUnknown:
+                check.add_message(Status.UNKNOWN, out)
+            else:
+                pass
+        
+        ##
+        ## Physical ports
+        ##   
+        port_count = Port.count_collection()
+        if port_count == 0:
+            check.add_message(Status.UNKNOWN,"no ports found on device")
+        port_count += fcport_count     
+        
+        Ports = Port.get_collection(fields="*")
+        for p in Ports:
             if (args.exclude or args.include) and item_filter(args,p.name):
                 logger.info(f"exclude port  {p.name}")
                 port_count -= 1
+                continue
+
+            if not hasattr(p, "enabled") or not p.enabled:
+                logger.info(f"Port isn't enabled {p.name}")
+                disabled += 1
                 continue
 
             logger.info(f"checking {p.node.name} - {p.name} - {p.type}")
@@ -101,15 +154,15 @@ def run():
                     check.add_message(Status.CRITICAL, out)
                 else:
                     check.add_message(Status.OK,out)
-
+        
     except NetAppRestError as error:
         check.exit(Status.UNKNOWN, "Error => {}".format(error))
     except Exception as error:
         logger.exception(error)
-
+    
     short = f"checked {port_count} Ports; ({port_count- disabled} enabled, {disabled} disabled)"
     (code, message) = check.check_messages(separator='\n')
     check.exit(code=code,message=f"{short}\n{message}")
-
+   
 if __name__ == "__main__":
     run()
